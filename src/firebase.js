ae7b07bfa853e365cd3dd30c6827a4eb05ce4f5e
aqsa khan <aqsa250bpl@outlook.com>
2022-12-04 17:45:33 +0530

    first commit

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCuhVzrPu0IvxN6X7cNAsgNlrNjnFdSp4M",
+  authDomain: "netflix-clone-a6e78.firebaseapp.com",
+  projectId: "netflix-clone-a6e78",
+  storageBucket: "netflix-clone-a6e78.appspot.com",
+  messagingSenderId: "156268429662",
+  appId: "1:156268429662:web:5e2799ba689d89ca10cfcf"
+};
